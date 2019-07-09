c1cec5770b76e3d8534b4fb4f72d736cfca6b6f1
gabyrodrigues <gabyrlima.99@gmail.com>
2019-07-08 23:21:32 -0300

    atualização em equipes

--- LabGrim/page-sobre.php
+++ LabGrim/page-sobre.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
-		<section id="page-sobre" class="container">
+		<section id="page-sobre">
 			<main>
 				<!-- begin oqe -->
-				<section class="m-t-2p">
+				<section class="m-t-2p container">
 					<div class="div-title-1">
 						<h4 class="text-center float-left">O QUE É?</h4>
 					</div>
@@ -13,7 +13,7 @@
 				<!-- end oqe -->
 
 				<!-- begin comocomecou -->
-				<section class="m-t-5p">
+				<section class="m-t-5p container">
 					<div class="div-title-2">
 						<h4 class="text-center float-left">COMO COMEÇOU...</h4>
 					</div>
@@ -30,9 +30,14 @@
 
 				<!-- begin equipe -->
 				<section class="m-t-5p">
-					<div class="div-title-3 align-center">
-						<h4 class="text-center float-left">NOSSA EQUIPE</h4>
+					<div class="div-box">
+						<div class="column rect_amarelo"></div>
+						<div class="column div-title-3 align-center">
+							<h4 class="text-center float-left">NOSSA EQUIPE</h4>
+						</div>
+						<div class="column triang_amarelo"></div>
 					</div>
+					
 
 	                <ul class="nav align-center w-100 m-t-5p">
 	                    <li class="inline-block p-r-1p">
@@ -51,6 +56,48 @@
 	                        <button class="navbar-button">BOLSISTAS ANTERIORES</button>
 	                    </li>
 	                </ul>
+
+	                <ul class="nav align-center w-100 m-t-5p">
+	                    <li class="inline-block text-center text-box">
+							<div class="image-cropper">
+								<img src="https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="avatar" class="profile-pic">
+							</div>
+
+							<b class="nome">Fulano da Silva</b>
+							<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim sapien, rutrum eu ultricies nec, eleifend finibus dui. Fusce pretium elit at dui ultricies, in vulputate lorem luctus. Suspendisse ornare orci sed rhoncus lobortis. Nunc ac egestas augue. </p>
+							<a href="#" class="link">- Link lattes -</a>
+	                    </li>
+	                    <li class="inline-block text-center text-box">
+	                        <div class="image-cropper">
+	                        	<img src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="avatar" class="profile-pic">
+							</div>
+
+	                        <b class="nome">Fulano da Silva</b>
+							<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim sapien, rutrum eu ultricies nec, eleifend finibus dui. Fusce pretium elit at dui ultricies, in vulputate lorem luctus. Suspendisse ornare orci sed rhoncus lobortis. Nunc ac egestas augue. </p>
+							<a href="#" class="link">- Link lattes -</a>
+	                    </li>
+	                    <li class="inline-block text-center text-box">
+	                        <div class="image-cropper">
+	                        	<img src="https://images.pexels.com/photos/718978/pexels-photo-718978.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="avatar" class="profile-pic">
+							</div>
+
+	                        <b class="nome">Fulano da Silva</b>
+							<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim sapien, rutrum eu ultricies nec, eleifend finibus dui. Fusce pretium elit at dui ultricies, in vulputate lorem luctus. Suspendisse ornare orci sed rhoncus lobortis. Nunc ac egestas augue. </p>
+							<a href="#" class="link">- Link lattes -</a>
+	                    </li>
+
+	                    <li class="inline-block text-center text-box">
+	                        <div class="image-cropper">
+	                        	<img src="https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="avatar" class="profile-pic">
+							</div>
+
+	                        <b class="nome">Fulano da Silva</b>
+							<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer enim sapien, rutrum eu ultricies nec, eleifend finibus dui. Fusce pretium elit at dui ultricies, in vulputate lorem luctus. Suspendisse ornare orci sed rhoncus lobortis. Nunc ac egestas augue. </p>
+							<a href="#" class="link">- Link lattes -</a>
+	                    </li>
+	                </ul>
+
+	                
 
 
 				</section>
